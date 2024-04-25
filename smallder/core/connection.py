6bc7becbe2dccfcaac9c834b74aab400c0fa2703
1mnoi <502472403@qq.com>
2024-04-25 15:17:31 +0800

    ✨spider新增mysql属性,自动创建mysql连接池

--- smallder/core/connection.py
+++ smallder/core/connection.py
@@ -1,9 +1,26 @@
+from urllib.parse import urlparse
+
 import redis
+from sqlalchemy import create_engine
 
 
-def from_setting(redis_url):
+def from_redis_setting(redis_url):
     """
     redis://:yourpassword@localhost:6379/0
     redis://localhost:6379/0
     """
     return redis.StrictRedis.from_url(redis_url)
+
+
+def from_mysql_setting(mysql_url):
+    if not mysql_url:
+        return
+    # 解析数据库URL
+    parsed_url = urlparse(mysql_url)
+    engine = create_engine(
+        f'mysql+pymysql://{parsed_url.username}:{parsed_url.password}@{parsed_url.hostname}:{parsed_url.port}/{parsed_url.path.lstrip("/")}',
+        echo=True,  # 打印SQL语句，便于调试
+        pool_size=100,
+        max_overflow=100
+    )
+    return engine
--- smallder/core/spider.py
+++ smallder/core/spider.py
@@ -1,6 +1,6 @@
 from loguru import logger
 from smallder import Request
-from smallder.core.connection import from_setting
+from smallder.core.connection import from_redis_setting, from_mysql_setting
 from smallder.core.engine import Engine
 
 
@@ -8,6 +8,7 @@ class Spider:
     name = "base"
     fastapi = True  # 控制内部统计api的数据
     server = None  # redis连接server
+    mysql_server = None
     redis_url = ""  # redis连接信息
     batch_size = 0  # 批次从redis中获取多少数据
     redis_task_key = ""
@@ -15,7 +16,15 @@ class Spider:
     log = logger
     thread_count = 0  # 线程总数
     retry: int = 3  # 重试次数
-    custom_settings = {}  # 定制配置
+    custom_settings = {
+        "middleware_settings": {
+            # "middleware.xxxx.xxx.xxxx": 100,
+            # "middleware.xxxx.xxxx.xxxxxx": 500,
+        },
+        "dupfilter_class": "",  # "dupfilter.xxxxx.xxxxxx",
+        "scheduler_class": "",  # "scheduler.xxxxx.xxxxxx"
+        "mysql": "",  # "mysql://xxx:xxxxx@host:port/db_name"
+    }  # 定制配置
 
     def setup_redis(self):
         if self.server is not None:
@@ -26,7 +35,13 @@ class Spider:
                 or self.redis_url.startswith("unix://")
         ):
             return
-        self.server = from_setting(self.redis_url)
+        self.server = from_redis_setting(self.redis_url)
+
+    def setup_mysql(self):
+        db_url = self.custom_settings.get("mysql", "")
+        if not db_url:
+            return
+        self.mysql_server = from_mysql_setting(db_url)
 
     def start_request(self):
         if not len(self.start_urls):
