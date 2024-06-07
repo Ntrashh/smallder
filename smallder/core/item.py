80bf0915128278edefb0deb5ffc33fdca63fb443
1mnoi <502472403@qq.com>
2024-06-07 14:04:22 +0800

    feat: ✨ item支持类和字典通用赋值

--- smallder/core/item.py
+++ smallder/core/item.py
@@ -1,14 +1,15 @@
 class Item(dict):
-    def __init__(self, *args, **kwargs):
-        super(Item, self).__init__(*args, **kwargs)
-        for key, value in self.items():
-            if isinstance(value, dict):
-                self[key] = Item(value)
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(f"'Item' object has no attribute '{key}'")
 
+    def __setattr__(self, key, value):
+        self[key] = value
 
-
-
-
-
-
-
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError(f"'Item' object has no attribute '{key}'")
